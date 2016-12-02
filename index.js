481a9844692ac8b8a0cd876587e03d921561bd63
Erin Bond <erin@erinebond.com>
2016-12-02 15:59:52 -0800

    Added the JS keyboard into the portfolio site

--- index.js
+++ index.js
@@ -2,6 +2,70 @@
 
 $(document).ready(function (){
 
+//Assignments page
+  $('#calc').show();
+  $('#solar-system').hide();
+  $('#bank').hide();
+  $('#farmer').hide();
+  $('#scrabble').hide();
+  $('#task').hide();
+  $('#etsy').hide();
+  $('#JS').hide();
+
+  $("#solar-system-link").click(function(){
+    $('#calc').hide();
+    $('#solar-system').show();
+    $('#bank').hide();
+    $('#farmer').hide();
+    $('#etsy').hide();
+    $('#JS').hide();
+  });
+
+  $("#calc-link").click(function(){
+    $('#calc').show();
+    $('#solar-system').hide();
+    $('#bank').hide();
+    $('#farmer').hide();
+    $('#etsy').hide();
+    $('#JS').hide();
+  });
+
+  $("#bank-link").click(function(){
+    $('#calc').hide();
+    $('#solar-system').hide();
+    $('#bank').show();
+    $('#farmer').hide();
+    $('#etsy').hide();
+    $('#JS').hide();
+  });
+
+  $("#farmer-link").click(function(){
+    $('#calc').hide();
+    $('#solar-system').hide();
+    $('#bank').hide();
+    $('#farmer').show();
+    $('#etsy').hide();
+    $('#JS').hide();
+  });
+
+  $("#etsy-link").click(function(){
+    $('#calc').hide();
+    $('#solar-system').hide();
+    $('#bank').hide();
+    $('#farmer').hide();
+    $('#etsy').show();
+    $('#JS').hide();
+  });
+
+  $("#JS-link").click(function(){
+    $('#calc').hide();
+    $('#solar-system').hide();
+    $('#bank').hide();
+    $('#farmer').hide();
+    $('#etsy').hide();
+    $('#JS').show();
+  });
+//Portfolio page
   $('.overlay').hide();
 
   $('.petsy img').mouseover(function () {
@@ -9,5 +73,6 @@ $(document).ready(function (){
   }).mouseout(function () {
       $('.overlay').hide();
   });
+
 
 });
